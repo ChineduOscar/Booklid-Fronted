65dec339b370c94d0fc98e14e369a39732a7ab05
chineduoscar <chinedunnadoziepatrick@gmail.com>
2026-06-26 06:03:46 +0100

    changed the backend url

--- src/lib/authAxios.js
+++ src/lib/authAxios.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export const authApi = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: "https://booklid-backend.onrender.com/api",
 });
 
 authApi.interceptors.request.use((config) => {
-  const token = Cookies.get('token');
+  const token = Cookies.get("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -19,10 +19,10 @@ authApi.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      Cookies.remove('token');
-      window.location.href = '/signin';
+      Cookies.remove("token");
+      window.location.href = "/signin";
     }
 
     return Promise.reject(error);
-  }
+  },
 );
