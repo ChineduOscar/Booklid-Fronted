5a462d455dc17e26e82ee4bb2f0c51837d317ca0
chineduoscar <chinedunnadoziepatrick@gmail.com>
2026-06-26 06:15:10 +0100

    fixed import of middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Books from './pages/Books';
-import SignIn from './pages/Signin';
-import Register from './pages/Register';
-import MainLayout from './layouts/MainLayout';
-import SingleBook from './pages/SingleBook';
-import Cart from './pages/Cart';
-import AdminLayout from './layouts/AdminLayout';
-import Dashboard from './pages/Admin/Dashboard';
-import Orders from './pages/Admin/Orders';
-import Users from './pages/Admin/Users';
-import Payments from './pages/Admin/Payments';
-import AllProducts from './pages/Admin/AllProducts';
-import AddProduct from './pages/Admin/AddProducts';
-import PaymentSuccess from './pages/paymentSuccess';
-import EditProduct from './pages/Admin/EditProduct';
-import ProtectedRoute from './middleWare/protectedRoute';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import SignIn from "./pages/Signin";
+import Register from "./pages/Register";
+import MainLayout from "./layouts/MainLayout";
+import SingleBook from "./pages/SingleBook";
+import Cart from "./pages/Cart";
+import AdminLayout from "./layouts/AdminLayout";
+import Dashboard from "./pages/Admin/Dashboard";
+import Orders from "./pages/Admin/Orders";
+import Users from "./pages/Admin/Users";
+import Payments from "./pages/Admin/Payments";
+import AllProducts from "./pages/Admin/AllProducts";
+import AddProduct from "./pages/Admin/AddProducts";
+import PaymentSuccess from "./pages/paymentSuccess";
+import EditProduct from "./pages/Admin/EditProduct";
+import ProtectedRoute from "./middleware/protectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/cart' element={<Cart/>}></Route>
-          <Route path='/books' element={<Books />}></Route>
-          <Route path='/book/:id' element={<SingleBook />}></Route>
-          <Route path='/payment/success' element={<PaymentSuccess />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/books" element={<Books />}></Route>
+          <Route path="/book/:id" element={<SingleBook />}></Route>
+          <Route path="/payment/success" element={<PaymentSuccess />}></Route>
         </Route>
 
         <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
@@ -41,12 +41,11 @@ function App() {
           </Route>
         </Route>
 
-
-        <Route path='/signin' element={<SignIn />}></Route>
-        <Route path='/register' element={<Register />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
